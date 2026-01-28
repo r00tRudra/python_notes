c8fcdcb82cd214fb087be933d75afd8e2f49f438
Rudra Bhowmick <bhowmickrudra07@gmail.com>
2026-01-28 23:54:45 +0530

    string operations
    Add string slicing examples and skip value demonstration.

---
+++ strings.py
@@ -0,0 +1,18 @@
+ab = "ab0123456789abcdefghijklmnopqrstuvwxyz"
+#slicing
+# print(ab[1:10])
+# print(ab[:5])
+# print(ab[1:])
+# print(ab[-10:-2])
+
+##skip value
+print(ab[1:10:2]) #selects in range of 1-10 with the skip value of 2
+
+#strig finctions
+#a = len(ab)
+#a = ab.startswith("012")
+#a = ab.endswith("789")
+#a = ab.title()
+#a = ab.capitalize()
+
+
